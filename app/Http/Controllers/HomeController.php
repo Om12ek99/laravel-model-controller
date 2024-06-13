db87cc27bf5e908b8c022111230ada418222836e
Om12ek99 <omar.elkamili@gmail.com>
2024-06-14 00:03:23 +0200

    add: public function index with link to model Movie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $moviesList = Movie::all();
+         return view("movie",compact("moviesList"));
+    }
 }
